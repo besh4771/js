4129a4f7d5887056c550d1e6b8d749e4fbdf10a8
Benja <benjaminokumu12@gmail.com>
2022-05-03 10:43:43 +0300

    js

--- index.js
+++ index.js
@@ -62,9 +62,14 @@
 // console.log(yourName);
 
 
-if (false != true) {
-    console.log("That makes sense");
-    if (1 < 2) {
-        console.log("No surprise there");
-    }
+// if (false != true) {
+//     console.log("That makes sense");
+//     if (1 < 2) {
+//         console.log("No surprise there");
+//     }
+// }
+
+
+for (let number = 0; number <= 12; number = number +2) {
+    console.log(number);
 }
