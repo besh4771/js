ad7abe609bca0ee94c1c67770020e2cd18f4683d
Benja <benjaminokumu12@gmail.com>
2022-05-03 13:17:13 +0300

    js

--- index.js
+++ index.js
@@ -105,14 +105,28 @@
 // }
 
 
-let i;
-let j;
-for (i = 0; i <= 5; i++){
-    document.write("</br>");
-    for (j = 0; j < 6 - i; j++){
-        document.write("&nbsp&nbsp");
-        for (j = 6 - i; j <= 5; j++){
-            document.write("*");
-        }
-    }
-}
+// let i;
+// let j;
+// for (i = 0; i <= 5; i++){
+//     document.write("</br>");
+//     for (j = 0; j < 6 - i; j++){
+//         document.write("&nbsp&nbsp");
+//         for (j = 6 - i; j <= 5; j++){
+//             document.write("*");
+//         }
+//     }
+// }
+
+// function leftTriangle(rows){
+//     let result = '';
+//     for(let i = rows; i > 0; i--){
+//         if(i===rows){
+//             result += '*'.repeat(i) + '\n';
+//         }else{
+//             let empty = rows - i;
+//             result += ' '.repeat(empty) + '*'.repeat(i)+ '\n'
+//         }
+//     }
+//     return result;
+// }
+// console.log(leftTriangle(5));
