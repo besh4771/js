4e71c0778694ad423daf0330c19f4c6f2ba09b40
Benja <benjaminokumu12@gmail.com>
2022-05-03 17:49:15 +0300

    js

--- functions.js
+++ functions.js
@@ -15,3 +15,31 @@ function minus(a, b){
 }
 console.log(minus(10));  // -10
 console.log(minus(10, 5));  // 5
+
+
+
+function wrapValue(n){
+    let local = n;
+    return () => local;
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+console.log(wrap2());
+
+
+function findSolution(target) {
+    function find(current, history) {
+        if (current == target) {
+        return history;
+    } else if (current > target) {
+        return null;
+    } else {
+        return find(current + 5, `(${history} + 5)`) ||
+                find(current * 3, `(${history} * 3)`);
+    }
+    }
+    return find(1, "1");
+}
+console.log(findSolution(24));
