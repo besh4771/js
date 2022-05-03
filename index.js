c229e530ed344e7939ac5440854a72315b1ef2fe
Benja <benjaminokumu12@gmail.com>
2022-05-03 10:54:59 +0300

    js

--- index.js
+++ index.js
@@ -70,6 +70,31 @@
 // }
 
 
-for (let number = 0; number <= 12; number = number +2) {
-    console.log(number);
+// for (let number = 0; number <= 12; number = number +2) {
+//     console.log(number);
+// }
+
+
+// for (let current = 20; ; current = current + 1) {
+//     if (current % 7 == 0) {
+
+//         console.log(current);
+//         break;
+//     }
+// }
+    
+
+switch (prompt("what is the weather like?")){
+    case "rainy":
+        console.log("remember to bring umbrella");
+        break;
+    case "sunny":
+        console.log("Dress lightly");
+        break;
+    case "cloudy":
+        console.log("Go outside");
+        break;
+    default:
+        console.log("unknown weather type!");
+        break;
 }
