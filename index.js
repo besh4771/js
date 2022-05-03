fcde7ad89d22a8b1bccda4a76209694c6b170153
Benja <benjaminokumu12@gmail.com>
2022-05-03 13:57:55 +0300

    js

--- index.js
+++ index.js
@@ -144,3 +144,37 @@ const makeNoise = function(){
 }
 makeNoise();
 
+const power = function(base, exponent){
+    let result = 1;
+    for(let count = 0; count < exponent; count++){
+        result *= base;
+    }
+    return result;
+}
+console.log(power(2, 10));
+
+
+let x = 10;
+if (true) {
+    let y = 20;
+    let z = 30;
+    console.log(x + y + z);
+}
+
+
+const hummus = function(factor) {
+    const ingredient = function(amount, unit, name){
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1){
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit} ${name}`);
+    };
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
+
+};
