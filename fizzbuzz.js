b9fcf14cd6972aec6c8c69eb365b0c5f1bc0d68f
Benja <benjaminokumu12@gmail.com>
2022-05-03 21:40:16 +0300

    js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,17 +7,17 @@
 // or "Buzz" for numbers divisible by only one of those).
 
 
-for (let i = 1; i <= 100; i++){
-    if (i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBuzz")
-    }else if(i % 3 == 0){
-        console.log("Fizz")
-    }else if(i % 5 == 0){
-        console.log("Buzz")
-    }else{
-        console.log(i)
-    }
-}
+// for (let i = 1; i <= 100; i++){
+//     if (i % 3 == 0 && i % 5 == 0){
+//         console.log("FizzBuzz")
+//     }else if(i % 3 == 0){
+//         console.log("Fizz")
+//     }else if(i % 5 == 0){
+//         console.log("Buzz")
+//     }else{
+//         console.log(i)
+//     }
+// }
 
 // for (var n = 1; n <= 100; n++) {
 //     var output = "";
@@ -27,3 +27,32 @@ for (let i = 1; i <= 100; i++){
 //       output += "Buzz";
 //     console.log(output || n);
 // }
+
+// function printFarmInventory(cows, chickens) {
+//     let cowString = String(cows);
+//     while(cowString.length < 3) {
+//         cowString = "0" + cowString;
+//     }
+//     console.log(`${cowString} Cows`);
+//     let chickenString = String(chickens);
+//     while(chickenString.length < 3) {
+//         chickenString = "0" + chickenString;
+//     }
+//     console.log(`${chickenString} Chickens`);
+// }
+// printFarmInventory(7, 11);
+
+function printZeroPaddedWithLabel(number, label) {
+    let numberString = String(number);
+    while (numberString.length < 3) {
+        numberString = "0" + numberString
+    }
+    console.log(`${numberString} ${label}`);
+}
+function printFarmInventory(cows, chickens, pigs) {
+    printZeroPaddedWithLabel(cows, "Cows");
+    printZeroPaddedWithLabel(chickens, "Chickens");
+    printZeroPaddedWithLabel(pigs, "Pigs");
+}
+
+printFarmInventory(7, 11, 3);
