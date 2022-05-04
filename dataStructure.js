14507c552ffe2ccb9c112a7a79a43a090ddbd0c7
Benja <benjaminokumu12@gmail.com>
2022-05-04 10:28:02 +0300

    js

---
+++ dataStructure.js
@@ -0,0 +1,13 @@
+// Methods
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+let sequence = [1, 2, 3];
+sequence.push(4);  // adds value to the end of an array
+sequence.push(5);
+console.log(sequence);
+
+console.log(sequence.pop())   // removes the last element from an array
+console.log(sequence)
+
