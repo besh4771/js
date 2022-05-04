91366ab0069ff69b7e564e58bf02b2253bb919f8
Benja <benjaminokumu12@gmail.com>
2022-05-04 10:56:16 +0300

    js

--- dataStructure.js
+++ dataStructure.js
@@ -11,3 +11,42 @@ console.log(sequence);
 console.log(sequence.pop())   // removes the last element from an array
 console.log(sequence)
 
+// Objects
+
+let day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running"]
+};
+console.log(day1.squirrel);
+
+let description = {
+    work: "Went to work",
+    "touched tree": "Touched a tree"
+};
+
+let anObject = {left: 1, right: 2};
+console.log(anObject.left);
+delete anObject.left;
+console.log(anObject.left);
+console.log("left" in anObject);
+console.log("right" in anObject);
+
+let objectA = {a: 1, b: 2};
+Object.assign(objectA, {b: 3, c: 4});
+console.log(objectA);
+
+let journol = [
+    {events: ["work", "touched tree", "pizza", "running", "television"],
+    squirrel: false},
+    {events: ["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"],
+    squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts", "beer"],
+    squirrel: true},
+];
+
+let journal = [];
+function addEntry(events, squirrel) {
+    journal.push({events, squirrel})
+}
+
+    
