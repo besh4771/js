4afafb9ee63f14841e64c7a20ebb4d70cbfb54ff
Benja <benjaminokumu12@gmail.com>
2022-05-05 11:29:04 +0300

    js

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -1,13 +1,34 @@
-function repeatLog(n){
-    for(let i = 0; i < n; i++){
-        console.log(i);
-    }
-}
+// function repeatLog(n){
+//     for(let i = 0; i < n; i++){
+//         console.log(i);
+//     }
+// }
 
-function repeat (n, action) {
-    for (let i = 0; i < n; i++) {
-        action(i);
-    }
-}
-repeat(3, console.log)
+// function repeat (n, action) {
+//     for (let i = 0; i < n; i++) {
+//         action(i);
+//     }
+// }
+// repeat(3, console.log)
 
+// let labels = [];
+// repeat(5, i=> {
+//     labels.push(`Unit ${i + 1}`);
+// });
+// console.log(labels);
+
+function greaterThan(n) {
+    return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan11(10));
+
+function noisy(f) {
+    return (...args) => {
+        console.log("calling with", args);
+        let result = f(...args);
+        console.log("calling with", args, ", returned", result);
+        return result;
+    };
+}
+noisy(Math.min)(3, 2, 1);
