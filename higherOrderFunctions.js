4b1ee73687b0ea0fa9a6107bd4b827e79f41f14c
Benja <benjaminokumu12@gmail.com>
2022-05-08 17:58:02 +0300

    js

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -86,3 +86,35 @@ console.log(Math.round(average(
 console.log(Math.round(average(
     SCRIPTS,filter(s => !s.living).map(s => s.year)
 )))
+
+
+function CountBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({name, count: 1});
+        }else{
+            counts[known]++;
+        }
+    }
+    return counts;
+}
+console.log(CountBy([1,2,3,4,5], n => n > 2));
+
+
+
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.name : "none";
+    }).filter(({name}) => name != "none");
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+    return scripts.map(({name, count}) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+}
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'))
+    
