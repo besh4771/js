890bd8785a818605bba1e74b9a2133ddf81c87ac
Benja <benjaminokumu12@gmail.com>
2022-05-08 18:15:07 +0300

    js

---
+++ Objects.js
@@ -0,0 +1,16 @@
+// methods
+let rabbit = {};
+rabbit.speak = function(line) {
+    console.log(`The rabbit says '${line}'`);
+};
+rabbit.speak("I'm alive.");
+
+function speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+}
+let whiteRabbit = {type: "white", speak};
+let hungryRabbit = {type: "hungry", speak};
+whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
+hungryRabbit.speak("I could use a carrot right now.");
+
+
