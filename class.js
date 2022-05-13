c2041bccc77e9ebe0d02d67cc1757d49bc6d4c76
Benja <benjaminokumu12@gmail.com>
2022-05-13 06:50:59 +0300

    js

--- class.js
+++ class.js
@@ -25,3 +25,28 @@ console.log(killerRabbit.teeth)
 // small
 
 console.log(Array.prototype.toString == Object.prototype.toString)
+// false
+
+
+//// MAPS
+// - A map is a data structure that associates values (the keys) with other values
+
+let ages = {
+    Boris: 39,
+    Liang: 22,
+    Julia: 62
+};
+console.log(`Julia is ${ages["Julia"]}`);
+// Julia is 62
+
+console.log("Is Jacks age known?", "Jack" in ages);
+// Is Jack's age known? false
+
+console.log("Is toString's age known?", "toString" in ages)
+// Is toString's age known? true
+
+
+let details = new Map();
+ages.set(39)
+details.set("Boris")
+details.set("Dwayne")
