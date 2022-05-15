ca3a1d2f56e333e1b55fb7b9a415af671e90efb5
Benja <benjaminokumu12@gmail.com>
2022-05-15 10:41:18 +0300

    js

--- codingninjas.js
+++ codingninjas.js
@@ -1,2 +1,21 @@
 var temp = 10;
 temp = "Coding Ninjas";
+
+// Declaring multiple variables
+var temp1 = 10, temp2 = 20, temp3 = "coding Ninjas";
+
+var temp;
+console.log(temp); // undefined
+
+var x = "Coding";
+var y = "Ninjas";
+var z = x + y;
+console.log(z);
+
+x = 1;
+y = 2;
+console.log(x++ + ++y);
+
+console.log(temp);
+var temp = "Coding Ninja";
+
