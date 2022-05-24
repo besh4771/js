f2c86dba232c3c0e38d423514cf25419dd954d0a
Benja <benjaminokumu12@gmail.com>
2022-05-24 12:09:25 +0300

    py

---
+++ grade.py
@@ -0,0 +1,102 @@
+# mark = []
+# tot = 0
+# print("Enter Number of Subjects: ")
+# subNo = int(input())
+# print("Enter Marks Obtained in " + str(subNo) + " Subjects: ")
+# for i in range(subNo):
+#     mark.append(input())
+
+# for i in range(subNo):
+#     tot = tot + int(mark[i])
+# avg = tot/subNo
+
+# if avg>=91 and avg<=100:
+#     print("Grade = A1")
+# elif avg>=81 and avg<91:
+#     print("Grade = A2")
+# elif avg>=71 and avg<81:
+#     print("Grade = B1")
+# elif avg>=61 and avg<71:
+#     print("Grade = B2")
+# elif avg>=51 and avg<61:
+#     print("Grade = C1")
+# elif avg>=41 and avg<51:
+#     print("Grade = C2")
+# elif avg>=33 and avg<41:
+#     print("Grade = D")
+# elif avg>=21 and avg<33:
+#     print("Grade = E1")
+# elif avg>=0 and avg<21:
+#     print("Grade = E2")
+# else:
+#     print("Invalid Input!")
+
+
+
+# marks = int(input("Enter marks for grading: "))
+
+# if marks>=70 and marks<=100:
+#     print("Grade A")
+# elif marks>=60 and marks<=69:
+#     print("Grade B")
+# elif marks>=50 and marks<=59:
+#     print("Grade C")
+# elif marks>=40 and marks>=49:
+#     print("Grade D")
+# elif marks>=0 and marks<=39:
+#     print("Grade F")
+# else:
+#     print("Not Valid")
+    
+    
+    
+    
+# marks = input("Enter Marks Obtained: ")
+# marks = int(marks)
+# if marks >= 80:
+#     grade = 'A'
+# else:
+#     if marks >= 70:
+#         grade = 'B'
+#     else:
+#         if marks >= 55:
+#             grade = 'C'
+#         else:
+#             if marks >= 50:
+#                 grade = 'Pass'
+#             else:
+#                  grade = 'Fail'
+# print ("\nYour Grade is: " + grade)
+
+
+
+
+# switch(score):
+#     case score > 90:
+#         return 'A'
+#     case score < 90 and score >= 80:
+#         return 'B'
+
+# 9
+        
+
+# marks = int(input("Enter marks for grading: "))
+# switch(marks):
+#     case marks>=70 and marks<=100:
+#         print("Grade A")
+#         break
+#     case marks>=60 and marks<=69:
+#         print("Grade B")
+#         break
+#     case marks>=50 and marks<=59:
+#         print("Grade C")
+#         break
+#     case marks>=40 and marks>=49:
+#         print("Grade D")
+#         break
+#     case marks>=0 and marks<=39:
+#         print("Grade F")
+#         break
+#     default:
+#         print("Not Valid")
+#         break
